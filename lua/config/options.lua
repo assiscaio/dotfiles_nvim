a70309eb0d738cc53dc5fad69e090ea9e6528fab
Caio Assis <assis.caio@outlook.com.br>
2025-07-16 16:44:50 -0300

    Set RNU mode

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,3 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
 vim.g.mapleader = ' '
 vim.o.wrap = false
 local vimdir = vim.fn.stdpath("config")
@@ -10,3 +7,4 @@ vim.o.smartindent = true
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.nu = true
+vim.o.rnu = true
